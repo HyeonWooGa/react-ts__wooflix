c34936db00d5bd7672b4015ca33eb39c4334fb58
HyeonWooGa <yeonwoopark22@gmail.com>
2022-08-17 22:24:28 +0900

    라우터 설정, 헤더 초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Serach";
+import Tv from "./Routes/Tv";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tv" element={<Tv />} />
+        <Route path="/serach" element={<Search />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
 import App from "./App";
@@ -78,7 +79,9 @@ root.render(
     <RecoilRoot>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </ThemeProvider>
     </RecoilRoot>
   </React.StrictMode>
