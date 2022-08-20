e7d7abd0a0374073e764527c4bd9c76d7ef8428a
HyeonWooGa <yeonwoopark22@gmail.com>
2022-08-20 17:57:02 +0900

    Slider 두 번 클릭시 애니매이션 중첩 문제 해결

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -75,7 +75,13 @@ function Home() {
     getMovies
   );
   const [index, setIndex] = useState(0);
-  const increaseIndex = () => setIndex((prev) => prev + 1);
+  const [leaving, setLeaving] = useState(false);
+  const increaseIndex = () => {
+    if (leaving) return;
+    setLeaving(true);
+    setIndex((prev) => prev + 1);
+  };
+  const toggleLeaving = () => setLeaving((prev) => !prev);
   // console.log(data, isLoading);
   return (
     <Wrapper>
@@ -91,7 +97,7 @@ function Home() {
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
           <Slider>
-            <AnimatePresence>
+            <AnimatePresence onExitComplete={toggleLeaving}>
               <Row
                 variants={rowVariants}
                 initial="hidden"
