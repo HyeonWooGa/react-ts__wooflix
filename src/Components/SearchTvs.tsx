1bbaab37837a706578f4ccb238d48a6b5fe30c02
HyeonWooGa <yeonwoopark22@gmail.com>
2023-11-05 20:57:38 +0900

    ✨ Seach 탭 기능 구현

---
+++ src/Components/SearchTvs.tsx
@@ -0,0 +1,259 @@
+import { useQuery } from "@tanstack/react-query";
+import { motion, AnimatePresence, useScroll } from "framer-motion";
+import { useState } from "react";
+import { PathMatch, useMatch, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { getSearchTvs, IGetTvsResult } from "../api";
+import { maekImagePath } from "../utills";
+
+const Wrapper = styled.div`
+  background-color: black;
+`;
+
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 5px;
+  grid-template-columns: repeat(6, 1fr);
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)<{ bgphoto: string }>`
+  background-color: white;
+  height: 200px;
+  font-size: 66px;
+  background-image: url(${(props) => props.bgphoto});
+  background-size: cover;
+  background-position: center center;
+  cursor: pointer;
+  &:first-child {
+    transform-origin: center left;
+  }
+  &:last-child {
+    transform-origin: center right;
+  }
+`;
+
+const Info = styled(motion.div)`
+  padding: 10px;
+  background-color: ${(props) => props.theme.black.lighter};
+  opacity: 0;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  h4 {
+    text-align: center;
+    font-size: 18px;
+  }
+`;
+
+const Overlay = styled(motion.div)`
+  position: fixed;
+  top: 0;
+  height: 100%;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  opacity: 0;
+`;
+
+const BigMovie = styled(motion.div)`
+  position: absolute;
+  width: 40vw;
+  height: 80vh;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  background-color: ${(props) => props.theme.black.lighter};
+  border-radius: 15px;
+  overflow: hidden;
+`;
+
+const BigCover = styled.div`
+  width: 100%;
+  height: 400px;
+  background-size: cover;
+  background-position: center center;
+`;
+
+const BigTitle = styled.h3`
+  color: ${(props) => props.theme.white.lighter};
+  padding: 20px;
+  font-size: 46px;
+  position: relative;
+  top: -80px;
+`;
+
+const BigOverview = styled.p`
+  padding: 20px;
+  position: relative;
+  top: -80px;
+  color: ${(props) => props.theme.white.lighter};
+`;
+
+const SizedBox = styled.div`
+  height: 280px;
+`
+
+const rowVariants = {
+  hidden: {
+    x: window.outerWidth + 5,
+  },
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: -window.outerWidth - 5,
+  },
+};
+
+const boxVariants = {
+  normal: {
+    scale: 1,
+    y: 0,
+  },
+  hover: {
+    scale: 1.3,
+    y: -50,
+    transition: {
+      delay: 0.4,
+      duration: 0.3,
+      type: "tween",
+    },
+  },
+};
+
+const infoVariants = {
+  hover: {
+    opacity: 1,
+    transition: {
+      delay: 0.4,
+      duration: 0.3,
+      type: "tween",
+    },
+  },
+};
+
+const offset = 6;
+
+function SearchTvs({keyword}: {keyword:string}) {
+  const navigate = useNavigate();
+  // bigMovieMatch 타입 선언 안줘 되지만 타입스크립트 공부를 위해 적용
+  const bigMovieMatch: PathMatch<string> | null = useMatch("/tvs/:tvId");
+  //console.log(bigMovieMatch);
+  const { scrollY } = useScroll();
+  const { data, isLoading } = useQuery<IGetTvsResult>(
+    ["tvs", "search"],
+    () => getSearchTvs(keyword as string)
+  );
+  const [index, setIndex] = useState(0);
+  const [leaving, setLeaving] = useState(false);
+  const increaseIndex = () => {
+    // 타입스크립트 오류 피하기 위해 if문 사용
+    if (data) {
+      if (leaving) return;
+      setLeaving(true);
+      const totalMovies = data.results.length - 1;
+      const maxIndex = Math.floor(totalMovies / offset) - 1;
+      setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+    }
+  };
+  const toggleLeaving = () => setLeaving((prev) => !prev);
+  const onBoxClicked = (tvId: number) => {
+    navigate(`/tvs/${tvId}`);
+  };
+  // console.log(data, isLoading);
+  const onOverlayClick = () => navigate(-1);
+  const clickedMovie =
+  bigMovieMatch?.params.tvId &&
+  data?.results.find(
+    (movie) => movie.id + "" === bigMovieMatch.params.tvId
+    );
+    console.log(clickedMovie);
+    return (
+      <Wrapper>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+        ) : (
+          <>
+          <SizedBox />
+          <Slider>
+            <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+              <Row
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: "tween", duration: 1 }}
+                key={index}
+              >
+                {data?.results
+                  .slice(1)
+                  .slice(offset * index, offset * index + offset)
+                  .map((movie) => (
+                    <Box
+                    bgphoto={maekImagePath(movie.poster_path, "w500")}
+                    key={movie.id}
+                    variants={boxVariants}
+                    initial="normal"
+                    whileHover="hover"
+                    transition={{ type: "tween" }}
+                    onClick={() => onBoxClicked(movie.id)}
+                    layoutId={movie.id + ""}
+                    >
+                      <Info variants={infoVariants}>
+                        <h4>{movie.name}</h4>
+                      </Info>
+                    </Box>
+                  ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
+          <AnimatePresence>
+            {bigMovieMatch ? (
+              <>
+                <Overlay
+                  onClick={onOverlayClick}
+                  exit={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                />
+                <BigMovie
+                  style={{ top: scrollY.get() + 100 }}
+                  layoutId={bigMovieMatch.params.tvId}
+                >
+                  {clickedMovie && (
+                    <>
+                      <BigCover
+                        style={{
+                          backgroundImage: `linear-gradient(to top, black, transparent), url(${maekImagePath(
+                            clickedMovie.backdrop_path,
+                            "w500"
+                          )})`,
+                        }}
+                      />
+                      <BigTitle>{clickedMovie.name}</BigTitle>
+                      <BigOverview>{clickedMovie.overview}</BigOverview>
+                    </>
+                  )}
+                </BigMovie>
+              </>
+            ) : null}
+          </AnimatePresence>
+        </>
+      )}
+    </Wrapper>
+  );
+}
+
+export default SearchTvs;
