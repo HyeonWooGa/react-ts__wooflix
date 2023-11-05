0575377497336d22ee6302886771b5f9c08fb8c7
HyeonWooGa <yeonwoopark22@gmail.com>
2023-11-05 20:54:45 +0900

    ✨ TV Shows 탭과 검색창을 위한 함수 추가

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-const API_KEY = "b9a221486250d0601edc387fbf688741";
+const API_KEY = "10923b261ba94d897ac6b81148314a3f";
 const BASE_PATH = "https://api.themoviedb.org/3";
 
 interface IMoive {
@@ -20,6 +20,25 @@ export interface IGetMoviesResult {
   total_results: number;
 }
 
+interface ITv {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  name: string;
+  overview: string;
+}
+
+export interface IGetTvsResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: ITv[];
+  total_pages: number;
+  total_results: number;
+}
+
 export function getMovies() {
   return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
     (response) => response.json()
@@ -34,6 +53,36 @@ export function getUpcomingMovies() {
 
 export function getTopRatedMovies() {
   return fetch(`${BASE_PATH}/movie/top_rated?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
+
+export function getTvs() {
+  return fetch(`${BASE_PATH}/tv/airing_today?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
+
+export function getOnTheAirTvs() {
+  return fetch(`${BASE_PATH}/tv/on_the_air?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
+
+export function getTopRatedTvs() {
+  return fetch(`${BASE_PATH}/tv/top_rated?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
+
+export function getSearchMovies(keyword: string) {
+  return fetch(`${BASE_PATH}/search/movie?api_key=${API_KEY}&query=${keyword}`).then(
+    (response) => response.json()
+  );
+}
+
+export function getSearchTvs(keyword: string) {
+  return fetch(`${BASE_PATH}/search/tv?api_key=${API_KEY}&query=${keyword}`).then(
     (response) => response.json()
   );
 }
